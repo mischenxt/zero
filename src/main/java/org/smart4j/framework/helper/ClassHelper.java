632db1a09a8f1d48cc9500b3b3775fd13912f9ae
79959 <79959@class3>
2019-08-02 17:41:43 +0800

    反射工具类和类帮助类

--- src/main/java/org/smart4j/framework/helper/ClassHelper.java
+++ src/main/java/org/smart4j/framework/helper/ClassHelper.java
@@ -1,8 +1,13 @@
 package org.smart4j.framework.helper;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.smart4j.framework.annotation.Controller;
+import org.smart4j.framework.annotation.Service;
 import org.smart4j.framework.util.ClassUtil;
+
 
 public final class ClassHelper {
 	/**
@@ -14,12 +19,42 @@ public final class ClassHelper {
 		CLASS_SET = ClassUtil.getClassSet(basePackage);
 	}
 	/**
-	 * 获取应用包名下的所有类
+	 * 获取应用包名下的所有Service类
 	 */
 	public static Set<Class<?>> getClassSet(){
 		return CLASS_SET;
 	}
 	
+	public static Set<Class<?>> getServiceClassSet(){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for(Class<?> cls :CLASS_SET) {
+			if(cls.isAnnotationPresent(Service.class)) {
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
+	/**
+	 * 获取应用名下所有Controller类
+	 */
+	public static Set<Class<?>> getControllerClassSet(){
+		Set<Class<?>> classSet = new HashSet<Class<?>>();
+		for(Class<?> cls :CLASS_SET) {
+			if(cls.isAnnotationPresent(Controller.class)) {
+				classSet.add(cls);
+			}
+		}
+		return classSet;
+	}
 	
+	/**
+	 * 获取应用名下所有Bean类（包括Service、Controller类）
+	 */
+	public static Set<Class<?>> getBeanClassSet(){
+		Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+		beanClassSet.addAll(getServiceClassSet());
+		beanClassSet.addAll(getControllerClassSet());
+		return beanClassSet;
+	}
 	
 }
---
+++ src/main/java/org/smart4j/framework/util/ReflectUtil.java
@@ -0,0 +1,8 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ReflectUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReflectUtil.class);
+}
