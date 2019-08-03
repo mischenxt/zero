c6388cd86e06c702ec8a5b7791e8287e0714dcdb
79959 <79959@class3>
2019-08-03 11:06:34 +0800

    controller

--- src/main/java/org/smart4j/framework/ConfigConstant.java
+++ src/main/java/org/smart4j/framework/ConfigConstant.java
@@ -9,4 +9,6 @@ public interface ConfigConstant {
 	String APP_BASE_PACKAGE="smart.framework.app.jsp_path";
 	String APP_JSP_PATH="smart.framework.app.jsp_path";
 	String APP_ASSET_PATH="smart.framework.app.asset_path";
+	
+	
 }
--- src/main/java/org/smart4j/framework/util/ReflectUtil.java
+++ src/main/java/org/smart4j/framework/util/ReflectUtil.java
@@ -1,8 +1,57 @@
 package org.smart4j.framework.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class ReflectUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReflectUtil.class);
+	/**
+	 * 创建实例
+	 * @param cls
+	 * @return
+	 */
+	public static Object newInstance(Class<?> cls) {
+		Object instance;
+		try {
+			instance = cls.newInstance();
+		} catch (Exception e) {
+			LOGGER.error("new intance failure" ,e );
+			throw new RuntimeException(e);
+		}
+		return instance; 
+	}
+	/**
+	 * 调用方法
+	 * @param obj
+	 * @param method
+	 * @param args
+	 * @return
+	 */
+	public static Object invokeMethod(Object obj , Method method, Object... args) {
+		Object result ; 
+		method.setAccessible(true);
+		try {
+			result = method.invoke(obj, args);
+		} catch (Exception e) {
+			LOGGER.error("invoke method failure" ,e );
+			throw new RuntimeException(e);
+		}
+		return result;
+	}
+	
+	public static void setfield(Object obj , Field field ,Object value) {
+		field.setAccessible(true);
+		try {
+			field.set(obj, value);
+		} catch (Exception e) {
+			LOGGER.error("invoke method failure" ,e );
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
 }
